5c2908d1d8f618736311a234b16411c415f4d7d3
stmcpeters <stmcpeters@gmail.com>
2025-04-13 20:40:18 -0700

    refactors index route to return JSON response

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_cors import CORS
 
 # creates Flask app instance
@@ -10,7 +10,9 @@ cors = CORS(app, origins='*')
 # creates a route for the root URL
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return {
+        'message': 'this is a message from the Flask backend!'
+    }
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
