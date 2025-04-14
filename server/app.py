fcb9651659eaecc948a0df12fb5e43568cf90035
stmcpeters <stmcpeters@gmail.com>
2025-04-13 20:07:52 -0700

    adds comments to app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
+# creates Flask app instance
 app = Flask(__name__)
 
+# enables cross-origin requests for all routes
+cors = CORS(app, origins='*')
 
+# creates a route for the root URL
 @app.route('/')
 def index():
     return render_template('index.html')
