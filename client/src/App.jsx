f7032d0bcc2e597c7c514327a35da3c2e9859160
stmcpeters <stmcpeters@gmail.com>
2025-04-13 20:41:08 -0700

    refactors App component to fetch and display data from server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState();
+
+  // function to fetch data using axios from server
+  const fetchAPI = async () => {
+    const response = await axios.get('http://127.0.0.1:8080/');
+    setResponse(response.data.message);
+  }
+
+  // useEffect will fetch data from server on initial page load
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome to VibeCheck!</h1>
+      <span>{response}</span>
     </>
   )
 }
