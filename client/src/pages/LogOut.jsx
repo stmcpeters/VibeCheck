16f41720613509a3bc48b562e6caa470019a24fe
stmcpeters <stmcpeters@gmail.com>
2025-04-21 23:23:08 -0700

    remove NavBar component from LogOut page

--- client/src/pages/LogOut.jsx
+++ client/src/pages/LogOut.jsx
@@ -1,11 +1,9 @@
 // log out successful page
 import React from 'react'
-import NavBar from '../components/NavBar'
 
 export default function LogOut() {
   return (
     <>
-      <NavBar />
       <div>You've succesfully logged out!</div>
     </>
   )
