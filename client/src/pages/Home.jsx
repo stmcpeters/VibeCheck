fd4e87daad2ed905b56b7ee8fe7b1e392fc0f48f
stmcpeters <stmcpeters@gmail.com>
2025-04-21 19:33:53 -0700

    remove NavBar component from Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Footer from "../components/Footer"
 export default function Home() {
   return (
     <>
-      <NavBar />
       <div
           className="hero min-h-screen"
           style={{
