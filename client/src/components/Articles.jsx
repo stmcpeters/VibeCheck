13c089405a1ae9fe42fb0e7fe0ab41fb7e7a8fbc
stmcpeters <stmcpeters@gmail.com>
2025-04-22 00:59:05 -0700

    update Articles component to display test article details in the table

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -20,9 +20,9 @@ export default function Articles() {
             {/* row 1 */}
             <tr className="hover:bg-base-300">
               <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
+              <td>Test Article</td>
+              <td>labs some fly night dig doctor bare band soft pitch plate bicycle gentle surface say deal next sugar exact moon over land log headingQuality Control Specialist</td>
+              <td>12min</td>
               <td className='pr-5 pl-5'>
                 <button className="btn btn-square">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>
