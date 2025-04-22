64dac8d223b8494b4f7252c6cff7c8b8ad07e205
stmcpeters <stmcpeters@gmail.com>
2025-04-22 00:53:35 -0700

    puts main elements on dashboard page with placeholders

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,41 @@
 // Landing page after login. Displays current mood prompt, mood streaks, and Tableau chart
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
-
+import MoodForm from "../components/MoodForm"
+import SearchBar from "../components/SearchBar"
+import Chart from "../components/Chart"
+import Articles from "../components/Articles"
+import LogEntry from "../components/LogEntry"
 
 export default function Dashboard() {
   return (
     <>
       <NavBar />
+      <SearchBar />
+      <br />
+      <hr />
+      <br />
+      <div className="flex w-full">
+        <div className="card rounded-box grid h-100 grow place-items-center">
+          <Chart />
+        </div>
+        <div className="divider divider-horizontal"></div>
+        <div className="card rounded-box grid h-150 grow place-items-center">
+          <MoodForm />
+        </div>
+      </div>
+      <hr />
+      <div className="flex w-full">
+        <div className="card rounded-box grid h-100 grow place-items-center">
+          <h1>Most Recent Web Scraped Articles</h1>
+          <Articles />
+        </div>
+        <div className="divider divider-horizontal"></div>
+        <div className="card rounded-box grid h-100 grow place-items-center">
+          <h1>Recent Mood Logs</h1>
+          <LogEntry />
+        </div>
+      </div>
       <Footer />
     </>
   )
