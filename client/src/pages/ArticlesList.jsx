989a0db1d8af43e1495d26f0080ae4fa19827f8b
stmcpeters <stmcpeters@gmail.com>
2025-04-22 00:14:01 -0700

    refactor ArticlesList component to include NavBar and Footer; add pagination structure

--- client/src/pages/ArticlesList.jsx
+++ client/src/pages/ArticlesList.jsx
@@ -1,8 +1,25 @@
 // Display  paginated list of web scraped articles with search and filter options
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import Articles from '../components/Articles'
 
 export default function ArticlesList() {
   return (
-    <div>ArticlesList</div>
+    <>
+      <NavBar />
+      <div className='place-items-center'>
+        <div className='grow text-2xl'>List of Articles</div>
+        <Articles />
+        {/* pagination */}
+        <div className="join">
+          <button className="join-item btn btn-disabled">«</button>
+          <button className="join-item btn">Page 1</button>
+          <button className="join-item btn">»</button>
+        </div>
+        {/* end of pagination */}
+      </div>
+      <Footer />
+    </>
   )
 }
