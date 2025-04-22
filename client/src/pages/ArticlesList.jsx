71bd8d1341cfc53e31f563997a13192b98a95f5c
stmcpeters <stmcpeters@gmail.com>
2025-04-21 19:46:15 -0700

    add ArticlesList component for displaying paginated web scraped articles

---
+++ client/src/pages/ArticlesList.jsx
@@ -0,0 +1,8 @@
+// Display  paginated list of web scraped articles with search and filter options
+import React from 'react'
+
+export default function ArticlesList() {
+  return (
+    <div>ArticlesList</div>
+  )
+}
