48b2e92fe4dbb4cd8e570819640598c52402565b
stmcpeters <stmcpeters@gmail.com>
2025-04-22 00:53:08 -0700

    refactor Chart component structure for improved readability and organization

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,1 +1,23 @@
 // Tableau visualization to show mood trends over time
+import React from "react";
+
+export default function Chart() {
+  return (
+    <>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img
+            src="https://external-preview.redd.it/933VNgu9xj8TMNZVqQjs-LWO8L-OCaYwwtNa2puko28.jpg?auto=webp&s=4ea8672c11446baa3619d6496f9d77a18c864283"
+            alt="placeholder chart"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Data Visualization</h2>
+          <p>
+            This card will display a chart to visualize data for the user
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
