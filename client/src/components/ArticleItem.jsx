2c92cd9dd02ca6c71cc0d46992474c5748d27201
stmcpeters <stmcpeters@gmail.com>
2025-04-30 18:17:00 -0700

    removes unused action buttons from ArticleItem component

--- client/src/components/ArticleItem.jsx
+++ client/src/components/ArticleItem.jsx
@@ -26,14 +26,6 @@ export default function ArticleItem({articles}) {
                   <td>{article.category}</td>
                   <td><a href={article.link} target='_blank' rel="noopener noreferrer">{article.title}</a></td>
                   <td>{article.author}</td>
-                  <td className='pr-5 pl-5'>
-                    <button className="btn btn-square">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>
-                    </button>
-                    <button className="btn btn-square">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
-                    </button>
-                  </td>
                 </tr>
               )
             )}
