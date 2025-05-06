f13e9a705e39bf72c232c9434e08d700d8951a04
stmcpeters <stmcpeters@gmail.com>
2025-05-05 20:44:29 -0700

    refactor LogOut component to enhance layout and add logout functionality

--- client/src/pages/LogOut.jsx
+++ client/src/pages/LogOut.jsx
@@ -1,10 +1,35 @@
 // log out successful page
+import axios from 'axios'
 import React from 'react'
-
+import Footer from '../components/Footer'
 export default function LogOut() {
+
+  // function to handle logout
+  const handleLogout = async (e) => {
+    try {
+      const response = await axios.post('/logout'); 
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  }
+
   return (
     <>
-      <div>You've succesfully logged out!</div>
+      <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: "url(https://preview.redd.it/missouri-clouds-are-unique-and-so-beautiful-no-edits-needed-v0-t7qmi11cu3ua1.jpg?width=1080&crop=smart&auto=webp&s=df1cdcc1780868ce05cfdacf911bc1e7eeddab2e)",
+          }}>
+          <div className="hero-overlay"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-2xl font-bold">You've succesfully logged out!</h1>
+              <button className="btn btn-primary"><a href="/">Back to Home</a></button>
+            </div>
+          </div>
+        </div>
+      <Footer />
     </>
   )
 }
