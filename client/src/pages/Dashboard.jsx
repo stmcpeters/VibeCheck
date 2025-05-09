53f3a7fc84ea27f80e6fa430707cab5151de7651
stmcpeters <stmcpeters@gmail.com>
2025-05-09 09:40:55 -0700

    Add loading state for articles in Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 // Landing page after login. Displays current mood prompt, mood streaks, and Tableau chart
+import React, { useState, useEffect } from "react"
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 import MoodForm from "../components/MoodForm"
@@ -8,12 +9,20 @@ import ArticleItem from "../components/ArticleItem"
 import MoodLogItem from "../components/MoodLogItem"
 
 export default function Dashboard({ mood_logs, articles }) {
+  const [loadingArticles, setLoadingArticles] = useState(true);
 
   // creates variable to hold the 2 most recent mood logs to be displayed on the dashboard
   const sortedMoodLogs = mood_logs.sort((a,b) => new Date(b.created_at) - new Date(a.created_at)).slice(0,2);
 
   // creates variable to hold the 2 articles to be displayed on the dashboard
   const featuredArticles = articles.slice(0,3);
+
+  // useEffect to set loadingArticles to false after articles are loaded
+  useEffect(() => {
+  if (articles.length > 0) {
+    setLoadingArticles(false);
+  }
+}, [articles]);
   
   return (
     <>
@@ -35,7 +44,11 @@ export default function Dashboard({ mood_logs, articles }) {
       <div className="flex w-full">
         <div className="card rounded-box grid h-100 grow place-items-center">
           <h1>Most Recent Web Scraped Articles</h1>
-          <ArticleItem articles={featuredArticles} />
+          {loadingArticles ? (
+            <p>Loading articles...</p>
+          ):(
+          <ArticleItem articles={featuredArticles} />)
+          }
         </div>
         <div className="divider divider-horizontal"></div>
         <div className="card rounded-box grid h-100 grow place-items-center">
